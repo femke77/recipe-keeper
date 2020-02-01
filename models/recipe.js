313fe90dca4c9a380fdb2058d179b496c68d637f
Meg Meyers <megan.g.meyers@gmail.com>
2020-02-01 14:46:11 -0800

    all code needed to pull from spoonacular api and insert properly into our own database

--- models/recipe.js
+++ models/recipe.js
@@ -10,22 +10,22 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     instructions: {
-      type: DataTypes.JSON,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     image: DataTypes.STRING,
     category: DataTypes.STRING,
     dishType: DataTypes.STRING,
-    postedBy: DataTypes.INTEGER,
+    source: DataTypes.STRING,
     userNotes: DataTypes.TEXT
   });
 
-  //We associate the user's id with the recipe's postedBy field
+  //We associate the user's id with the recipe's source field
   //The relationship is that one user may post many recipes to the site
   Recipe.associate = function(models) {
     Recipe.belongsTo(models.User, {
       foreignKey: {
-        allowNull: false
+        allowNull: true
       }
     });
   };
