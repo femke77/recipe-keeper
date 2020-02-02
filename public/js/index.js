9d25c301e3ec848c26934288fa4f94c234fd5dec
Meg Meyers <megan.g.meyers@gmail.com>
2020-02-01 22:11:40 -0800

    added post route for creating new recipe

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,16 @@
-//ajax here with js dom manuipulation as needed
+//jquery pull newRecipe data from user input
+let newRecipe = {
+  title: title,
+  servings: servings,
+  image: image,
+  category: category,
+  dishType: dishType,
+  source: source,
+  instructions: instructions,
+  ingredients: ingredients
+};
+
+//send to database with post
+$.post("/db/newrecipe", newRecipe, function() {
+  console.log("posted");
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,4 +3,17 @@ var express = require("express");
 
 var router = express.Router();
 
+// var { Recipe } = require("../models");
+
+// //Post a new recipe to the database
+// router.post("/db/newrecipe", (req, res) => {
+//   if (!req.body) {
+//     res.status("400").send("req body is required.");
+//     return;
+//   }
+//   Recipe.create(req.body).then(function() {
+//     res.status(200).end();
+//   });
+// });
+
 module.exports = router;
