f6d373922222cc84a434c9a3a431f77d204d2fc7
Meg Meyers <megan.g.meyers@gmail.com>
2020-02-04 19:13:58 -0800

    router for hbs render 3 random recipes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,16 @@ var express = require("express");
 
 var router = express.Router();
 
+var { Recipe } = require("../models");
+
+//will send 3 random recipes as a combined object to handlebars for display on landing page
+router.get("/", (req, res) => {
+  Recipe.findAll({
+    order: sequelize.random(),
+    limit: 3
+  }).then(function(recipes) {
+    res.render("index", recipes);
+  });
+});
+
 module.exports = router;
