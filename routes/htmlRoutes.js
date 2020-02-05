7f2741a40913d66165d4014d853a094e665f336c
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-02-04 22:17:20 -0800

    migrated html to handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,21 @@ var express = require("express");
 
 var router = express.Router();
 
+var db = require("../models");
+
+//on landing page, render the index.hbs file with 3 random recipes from the db
+router.get("/", (req, res) => {
+    db.Recipe.findAll({
+        order: db.sequelize.random(),
+        limit: 1
+    }).then(function (recipes) {
+        var hbsObject = {
+            recipes: recipes
+        };
+        console.log(recipes);
+        res.render("index", hbsObject);
+        console.log("hbsObject sent to handlebars index");
+    });
+});
+
 module.exports = router;
