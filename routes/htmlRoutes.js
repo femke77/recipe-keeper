916c67e2e3095f5ffd8d801a66485124bdc6963c
Kfriedman <Koryf91@gmail.com>
2020-02-07 17:27:10 -0800

    added the route to dash to show user

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,8 @@ var express = require("express");
 //var app = require("../server");
 var router = express.Router();
 var db = require("../models");
+var passport = require("passport");
+
 var exports = (module.exports = {});
 
 function isLoggedIn(req, res, next) {
@@ -15,9 +17,9 @@ function isLoggedIn(req, res, next) {
 exports.signup = function(req, res) {
   res.render("signup");
 };
-router.get("/test", isLoggedIn, function(req, res) {
+router.get("/dashboard", isLoggedIn, function(req, res) {
   console.log(req.user);
-  res.send("Hello");
+  res.send("Hello" + req.user.firstName);
 });
 
 //on landing page, render the index.hbs file with x number of  random recipes from the db
@@ -53,6 +55,52 @@ router.get("/recipe", (req, res) => {
 
 router.get("/signup", (req, res) => {
   res.render("signup");
+});
+
+router.post("/signup", function(req, res, next) {
+  passport.authenticate("signup", function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // console.log( 'User not registered' );
+      return res.redirect("/signup");
+    }
+    // console.log( 'User registered!' );
+    return res.redirect("/dashboard");
+    //res.redirect( '/' );
+  })(req, res, next);
+});
+router.get("/login", function(req, res) {
+  res.render("login");
+});
+
+router.post("/", function(req, res, next) {
+  passport.authenticate("login", function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      console.log("User not authenticated");
+      return res.redirect("/user/login");
+    }
+    req.logIn(user, function(loginerr) {
+      if (loginerr) {
+        console.log("Error while login: " + loginerr);
+        return next(loginerr);
+      }
+      req.session.messages = "Login successfull";
+      req.session.authenticated = true;
+      req.authenticated = true;
+      return res.redirect("/test");
+    });
+  })(req, res, next);
+});
+router.get("/logout", function(req, res) {
+  // eslint-disable-next-line no-unused-vars
+  req.session.destroy(function(err) {
+    res.redirect("/");
+  });
 });
 
 router.get("*", (req, res) => {
